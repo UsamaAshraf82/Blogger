8b7afc8317bd1e65782c279b5d862e1fac2412ce
Muhammad Usama Ashraf <32302712+UsamaAshraf82@users.noreply.github.com>
2017-12-18 22:23:46 +0500

    Header Added

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+
+    @include("Look//header");
+
+</body>
+</html>
