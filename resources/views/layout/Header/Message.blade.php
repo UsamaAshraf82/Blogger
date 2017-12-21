8eaba28e922db898c9d83c103938efc2c06d1082
Jahanzaib Maqsood <33222862+Jahanzaib-jb@users.noreply.github.com>
2017-12-21 19:27:15 +0500

    from me

--- resources/views/layout/Header/Message.blade.php
+++ resources/views/layout/Header/Message.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid" style="background-color: black; padding-top: 20px">
 
-    <div class="text-center"><p><strong>We are Anonymous, We are Legend</strong></p> </div>
+    <div class="text-center"><p><strong>Hamza Gandu teri bhund marni kam ker le changa rau ga</strong></p> </div>
     <br>
 </div>
